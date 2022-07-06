0adb8a716726498f0470db52bfe9ce57aecc8a68
ryosuke.mori <ryosuke.mori@nickellab.net>
2022-07-06 22:22:51 +0000

    add docker-compose

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
-    for n in 0..10 {
-        print!("{n}: Hello, Rust!")
-    }
+    let a = 10;
+    let b = 20;
+    let mut c = a;
+    c = 30;
+
+    // let ref_a1 = &a;
+    // let ref_a2 = &a;
+
+    println!("{} {} {}", a, b, c);
 }
